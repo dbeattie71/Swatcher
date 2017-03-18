7f0f6df4af1c0c2d68d0933a1c793f377d60030f
Martin Mizzell <martin@mizzellconsulting.com>
2017-03-18 16:03:20 -0500

    Tinkering with build command line.

--- src/build.bat
+++ src/build.bat
@@ -14,7 +14,7 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Swatcher\Swatcher.csproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Swatcher.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
 mkdir Build\lib
