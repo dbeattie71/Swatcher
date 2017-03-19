037838da4ed01bd72d97751e02faf6ed5b21a80f
Martin Mizzell <martin@mizzellconsulting.com>
2017-03-19 11:51:19 -0500

    Updating the build file to point to custom nuget

--- src/build.bat
+++ src/build.bat
@@ -9,12 +9,10 @@ if not "%PackageVersion%" == "" (
    set version=%PackageVersion%
 )
 
-set nuget=
-if "%nuget%" == "" (
-	set nuget=nuget
-)
+set nuget=nuget\nuget.exe
 
 nuget restore src\Swatcher.sln
+
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Swatcher.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
